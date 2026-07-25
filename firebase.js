32958c650de92d9e91c39ca7ff172d7e9e7d7472
danielidoghe-beep <danielidoghe@gmail.com>
2026-07-25 21:30:12 +0100

    Remove Firebase Analytics initialization
    Removed analytics import and initialization from firebase.js.

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -15,8 +16,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
+
 const db = getFirestore(app);
 
 export { auth, db };
