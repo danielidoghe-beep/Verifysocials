987e3acf1b290755c0b8304b0f3be053aab0cbf0
danielidoghe-beep <danielidoghe@gmail.com>
2026-07-25 21:42:27 +0100

    Implement logout functionality with signOut

---
+++ logout.js
@@ -0,0 +1,34 @@
+import { auth } from "./firebase.js";
+
+import {
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const logoutBtn =
+  document.getElementById("logoutBtn");
+
+if(logoutBtn){
+
+  logoutBtn.addEventListener(
+    "click",
+    async (e) => {
+
+      e.preventDefault();
+
+      try {
+
+        await signOut(auth);
+
+        window.location.href =
+          "login.html";
+
+      } catch(error) {
+
+        alert(error.message);
+
+      }
+
+    }
+  );
+
+}
